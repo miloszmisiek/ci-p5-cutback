412e4bb79d13af7c5555d69e5a91453582f9949f
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-09-08 14:04:36 +0200

    Edit axios defaults baseURL with correct value

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://cutback-project.herokuapp.com/';
+axios.defaults.baseURL = 'https://drf-api-cutback.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
