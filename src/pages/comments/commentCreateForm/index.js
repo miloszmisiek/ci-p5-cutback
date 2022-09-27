48c963da86a4cf6ccf57dd2b79335560e3d88cc1
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-09-27 22:26:12 +0200

    Add axiosRes import to commentCreateForm

--- src/pages/comments/commentCreateForm/index.js
+++ src/pages/comments/commentCreateForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import { useParams } from "react-router-dom";
+import { axiosRes } from "../../../api/axiosDefaults";
 
 import {
   CommentInput,
